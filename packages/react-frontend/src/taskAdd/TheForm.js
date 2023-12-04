855ce0620f830f509e75eb7adbe7ccce1564e529
Muhammad8493 <97069972+Muhammad8493@users.noreply.github.com>
2023-12-04 12:42:52 -0800

    Add task form validation (#33)
    * working task validation which works to prevent overlapping tasks, need to work on past midnight tasks
    
    * task time conflict validation works, tasks can't go past midnight
    
    ---------
    
    Co-authored-by: Muhammad Arshad <muhammadarshad@Muhammads-MacBook-Pro-3.local>

--- packages/react-frontend/src/taskAdd/TheForm.js
+++ packages/react-frontend/src/taskAdd/TheForm.js
@@ -24,39 +24,134 @@ function TheForm(props) {
         }));
     }
 
-    const submitForm = async () => {
-        try {
-            const response = await fetch(
-                `${api_url}/task-lists/65553647a73a1b75066a47ab/tasks`,
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(person)
-                }
-            );
+    const onSave = () => {
+        // Check if name is given
+        if (person.name === "") {
+            alert("Please give a name for the task.");
+            return;
+        }
+        // Check if priority is selected
+        if (person.description === "") {
+            alert("Please give a description for the task.");
+            return;
+        }
+        // Check if priority is selected
+        if (person.priority === "") {
+            alert("Please select a priority for the task.");
+            return;
+        }
+        // Check if priority is selected
+        if (person.date === "") {
+            alert("Please select a date for the task.");
+            return;
+        }
+        // Check if priority is selected
+        if (person.duration === "") {
+            alert("Please select a duration for the task.");
+            return;
+        } else if (parseInt(person.duration) < 30) {
+            alert("PLease enter a longer duration.");
+            return;
+        }
+        submitForm();
+    };
 
-            if (response.status === 201) {
-                const newUser = await response.json();
-                props.handleSubmit(newUser);
+    const submitForm = async () => {
+        person.date = new Date(person.date);
+        try {
+            // Wait for the promise
+            const timeConflict = await validateTask();
+            if (timeConflict) {
+                alert("Time conflict");
             } else {
-                console.error("Failed to add user");
+                try {
+                    const response = await fetch(
+                        `${api_url}/task-lists/65553647a73a1b75066a47ab/tasks`,
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify(person)
+                        }
+                    );
+                    if (response.status === 201) {
+                        const newUser = await response.json();
+                        props.handleSubmit(newUser);
+                    } else {
+                        console.error("Failed to add user");
+                    }
+                } catch (error) {
+                    console.error("Error adding user:", error);
+                }
+                // Reset the form after submission
+                setPerson({
+                    name: "",
+                    description: "",
+                    priority: "",
+                    tags: "",
+                    date: "",
+                    duration: ""
+                });
             }
         } catch (error) {
-            console.error("Error adding user:", error);
+            console.error("Error during form submission", error);
         }
-
-        // Reset the form after submission
-        setPerson({
-            name: "",
-            description: "",
-            priority: "",
-            tags: "",
-            date: "",
-            duration: ""
-        });
     };
+
+    function fetchTasks() {
+        const promise = fetch(`${api_url}/task-lists/65553647a73a1b75066a47ab`);
+        return promise;
+    }
+
+    async function validateTask() {
+        try {
+            const res = await fetchTasks();
+            // Wait for the promise from res.json() to resolve
+            const json = await res.json();
+
+            if (!json.tasks || !Array.isArray(json.tasks)) {
+                return false;
+            }
+
+            const newTaskStartTime = new Date(person.date);
+            const newTaskEndTime = new Date(person.date);
+            newTaskEndTime.setMinutes(
+                newTaskEndTime.getMinutes() + parseInt(person.duration)
+            );
+
+            // Check if the new task goes past midnight
+            const midnight = new Date(newTaskStartTime);
+            midnight.setHours(24, 0, 0, 0);
+
+            if (newTaskEndTime > midnight) {
+                console.error("Task goes past midnight, not allowed.");
+                return true;
+            }
+
+            for (let task of json.tasks) {
+                if (task.date) {
+                    const taskStartTime = new Date(task.date);
+                    const taskEndTime = new Date(task.date);
+                    taskEndTime.setMinutes(
+                        taskEndTime.getMinutes() + parseInt(task.duration)
+                    );
+
+                    // Check for overlap with other tasks
+                    if (
+                        newTaskStartTime < taskEndTime &&
+                        newTaskEndTime > taskStartTime
+                    ) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } catch (error) {
+            console.error("Error in validateTask: ", error);
+            return false;
+        }
+    }
 
     return (
         <Form>
@@ -66,6 +161,7 @@ function TheForm(props) {
                     type="text"
                     name="name"
                     placeholder="Enter name"
+                    maxLength="20"
                     value={person.name}
                     onChange={handleChange}
                 />
@@ -77,6 +173,7 @@ function TheForm(props) {
                     type="text"
                     name="description"
                     placeholder="Enter description"
+                    maxLength="50"
                     value={person.description}
                     onChange={handleChange}
                 />
@@ -89,6 +186,7 @@ function TheForm(props) {
                     name="tags"
                     placeholder="Enter tags"
                     value={person.tags}
+                    maxLength="50"
                     onChange={handleChange}
                 />
             </Form.Group>
@@ -96,12 +194,16 @@ function TheForm(props) {
             <Form.Group controlId="formBasicPriority">
                 <Form.Label>Priority</Form.Label>
                 <Form.Control
-                    type="text"
+                    as="select"
                     name="priority"
-                    placeholder="Enter priority"
                     value={person.priority}
                     onChange={handleChange}
-                />
+                >
+                    <option value="">Select Priority</option>
+                    <option value="1">High</option>
+                    <option value="2">Medium</option>
+                    <option value="3">Low</option>
+                </Form.Control>
             </Form.Group>
 
             <Form.Group controlId="formBasicDate">
@@ -118,16 +220,18 @@ function TheForm(props) {
             <Form.Group controlId="formBasicDuration">
                 <Form.Label>Duration (In Minutes) </Form.Label>
                 <Form.Control
-                    type="text"
+                    type="number"
                     name="duration"
+                    step="1"
                     placeholder="Enter duration"
+                    maxLength="5"
                     value={person.duration}
                     onChange={handleChange}
                 />
             </Form.Group>
 
             <div style={{ marginBottom: "10px" }}>
-                <Button variant="primary" onClick={submitForm}>
+                <Button variant="primary" onClick={onSave}>
                     Submit
                 </Button>
             </div>
