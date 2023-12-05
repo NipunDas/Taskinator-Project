c97164e069d5b740cd306f2ed058733610869a69
Nipun Das <nkdas@calpoly.edu>
2023-12-04 17:30:05 -0800

    Updated API_URL to deployed version and updated lock file

--- packages/react-frontend/src/Consts.js
+++ packages/react-frontend/src/Consts.js
@@ -2,5 +2,5 @@
 
 // First URL is deployed version, second is locally hosted version
 // Use first URL if deploying!
-//export const API_URL = "https://taskinator-api.azurewebsites.net";
-export const API_URL = "http://localhost:8000";
+export const API_URL = "https://taskinator-api.azurewebsites.net";
+//export const API_URL = "http://localhost:8000";
